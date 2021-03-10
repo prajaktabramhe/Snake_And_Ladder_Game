d689e680b48678313b1d029b48b53fefaf83cf8a
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-10 23:29:07 +0530

    [Prajakta]: used Random Function for new position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,24 @@
- public class SnakeLadder {
+import java.util.Random;
+ public class SnakeLadder 
+{
     public static final int startPosition = 0;
     public static final int winningPosition = 100;
+    public static int position=0;
+
+    public static int throwDie() 
+    {
+	Random random=new Random();
+        int value = random.nextInt(6)+1;
+	return value;
+     }
+
     public static void main(String[] args) 
     {
        System.out.println("Welcome to Snake and ladder game");
        System.out.println("Start position is: "  + startPosition);
        System.out.println("Winning position is: "  + winningPosition);
+       position = position + throwDie();
+       System.out.println("new position is : " +position);
+
      }
  }
