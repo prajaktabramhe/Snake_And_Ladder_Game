648bbfeb5f497b6250590d67919f0a72bc78f535
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-10 19:20:29 +0530

    [Prajakta]: Start and winning position

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+ public class SnakeLadder {
+    public static final int startPosition = 0;
+    public static final int winningPosition = 100;
+    public static void main(String[] args) 
+    {
+       System.out.println("Welcome to Snake and ladder game");
+       System.out.println("Start position is: " + " " + startPosition);
+       System.out.println("Winning position is: " + " " + winningPosition);
+     }
+ }
