2c205065d0d88d34cc504a496d3ca1fb67a083db
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-11 00:43:37 +0530

    [Prajakta]: Make dice roll count Zero after every play

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,10 @@
-import java.util.Random;
+ import java.util.Random;
  public class SnakeLadder 
 {
     public static final int startPosition = 0;
     public static final int winningPosition = 100;
     public static int position=0;
+    static int diceRollCount=0;
 
     public static int throwDie() 
     {
@@ -18,15 +19,14 @@ import java.util.Random;
 	 return  value1 ;
       }
 
-      public static void main(String[] args) 
+      static void play() 
       {
-         System.out.println("Welcome to Snake and ladder game");
-         System.out.println("Start position is: "  + startPosition);
-         System.out.println("Winning position is: "  + winningPosition);
+
        while(position < winningPosition) 
        {
           int playValue = optionForPlay();
           int diceRollValue = throwDie();
+          System.out.println("dice count is: "+diceRollCount);
 	  System.out.println("~~~ Dice rolled value: "+diceRollValue);
 
 
@@ -38,16 +38,15 @@ import java.util.Random;
           else if(playValue==1)
           {
              System.out.println("Got Ladder");
-             position = position + diceRollValue;
+             position = position += diceRollValue;
              if(position > 100) {
-
-		//System.out.println("congratulations player reached to winning position");
+               position -= diceRollValue;
 	     }
           }
           else if(playValue==2)
           {
              System.out.println("Snake Attack");
-             position = position - diceRollValue;
+             position = position -= diceRollValue;
              if(position < 0) 
               {
 		position=0;
@@ -58,7 +57,16 @@ import java.util.Random;
           {
 	      System.out.println("congratulations player reached to winning position");
 	      System.out.println("Game ended");
+              diceRollCount=0;
 	  }
         }
      }
- }
+     public static void main(String[] args){
+         System.out.println("Welcome to Snake and ladder game");
+         System.out.println("Start position is: "  + startPosition);
+         System.out.println("Winning position is: "  + winningPosition);
+         System.out.println("initial Dice roll count is: " + diceRollCount);
+         
+         play(); 
+     }
+}
