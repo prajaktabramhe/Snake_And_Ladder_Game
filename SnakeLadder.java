86afe28820540f71390c62e676ac39deb1d6d829
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-10 23:47:54 +0530

    [Prajakta]: used random method too cheked for options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -11,14 +11,36 @@ import java.util.Random;
         int value = random.nextInt(6)+1;
 	return value;
      }
+     public static int optionForPlay() 
+     {
+	 Random random=new Random();
+         int value1 = random.nextInt(3);
+	 return  value1 ;
+      }
 
-    public static void main(String[] args) 
-    {
-       System.out.println("Welcome to Snake and ladder game");
-       System.out.println("Start position is: "  + startPosition);
-       System.out.println("Winning position is: "  + winningPosition);
-       position = position + throwDie();
-       System.out.println("new position is : " +position);
+      public static void main(String[] args) 
+      {
+         System.out.println("Welcome to Snake and ladder game");
+         System.out.println("Start position is: "  + startPosition);
+         System.out.println("Winning position is: "  + winningPosition);
+        
+         int playValue=optionForPlay();
+         if(playValue==0) 
+         {
+            System.out.println("No play");
+            position = position + throwDie();
+         }
+         else if(playValue==1)
+         {
+            System.out.println("Got Ladder");
+            position = position + throwDie();
+         }
+         else if(playValue==2)
+         {
+            System.out.println("Snake Attack");
+            position = position + throwDie();
+         }
+         System.out.println("new position is : " +position);
 
      }
  }
